9be09dd82b764fc5b927061ced0b289866201395
benstackler <ben.stackler@gmail.com>
2025-11-11 13:31:27 -0500

    Update HallOfFame.sql

--- HallOfFame.sql
+++ HallOfFame.sql
@@ -37,19 +37,3 @@ inducted text, category text, needed_note text);
 # Join tables together so that we can find Hall of Fame player profiles inducted after 1990
 
 select * from hof left join master on hof.playerid = master.playerid where hof.inducted = 'Y' and yearid > 1990 order by yearid desc;
-
-# Let's limit our results to player name, year, and voting committee
-
- Select namefirst, namelast, yearid, votedby  from hof left join master on 
-
- # Now let's combine our name columns using the CONCAT function
-
- select CONCAT(namefirst, ' ', namelast) as fullname, yearid, votedby  from hof left join master on 
-hof.playerid = master.playerid where hof.inducted = 'Y' and yearid > 1990 order by yearid desc;
-
-# Let's add the percentage of votes received by each HOF inductee (approval percentage) by converting our integers into decimals, rounding, and ensuring zero ballot cases do not cause errors
-
-
-select CONCAT(namefirst, ' ', namelast) as fullname, yearid, votedby, 
- ROUND(CAST(votes AS DECIMAL(10,2)) / NULLIF(ballots, 0) * 100, 2) AS approval_percentage  from hof left join master on 
-hof.playerid = master.playerid where hof.inducted = 'Y' and yearid > 1990 order by yearid desc;
